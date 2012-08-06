0751ae8309821f95434f8801abd8c27e46b29ce2
Evgeny Martynov <evgeny@epochfail.com>
2012-08-06 20:58:42 +1000

    Lab04: Added lists.h

---
+++ lab04/lists.h
@@ -0,0 +1,18 @@
+typedef struct _node *link;
+
+link newNode (Item);
+void freeNode (link);
+
+void insertNode (link ls, Item item);
+void insertNext (link, link);
+link insertHead (link, link);
+link deleteNext (link);
+
+link getNext (link); 
+Item getItem (link);
+int isEmptyList (link);
+
+void square (link ls);
+void squareAll (link ls);
+
+int count (link ls, int (*pred)(Item));
