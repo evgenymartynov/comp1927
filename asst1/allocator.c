f7981267a8cfff6a256863381dcd996d38e77231
Evgeny Martynov <evgeny@epochfail.com>
2012-08-08 22:58:09 +1000

    Add Asst1, milestone 2 code

--- asst1/allocator.c
+++ asst1/allocator.c
@@ -11,7 +11,8 @@
 
 // Local defines
 #define MIN_ALLOCATOR_SIZE  sizeof(header)
-#define HEADER_MAGIC        0xBADA110C
+#define HEADER_FREE_MAGIC   0xBADA110C
+#define HEADER_USED_MAGIC   0xCAFFE14E
 
 
 // Local typedefs
@@ -34,8 +35,17 @@ static Header freelist_head = NULL;
 //
 
 static size_t round_up_power_of_two(size_t size);
-static void freelist_init(size_t size);
+static int chunk_is_free(Header chunk);
+static int __attribute__((unused)) chunk_is_used(Header chunk);
+static Header chunk_create(void* where, size_t size);
+static void* get_user_memory(Header chunk);
 
+static void   freelist_init(size_t size);
+static Header freelist_bestfit(size_t size);
+static void   freelist_split_chunk(Header chunk, size_t size);
+static int    freelist_has_one_chunk(void);
+static void   freelist_extract_chunk(Header chunk);
+static void   freelist_print(void);
 
 ////////////////////////////////////////////////////////////////////////
 //
@@ -43,8 +53,9 @@ static void freelist_init(size_t size);
 //
 ////////////////////////////////////////////////////////////////////////
 
+
 // Create a new allocator with a given minimal size.
-void allocator_init(u_int32_t size) {
+void allocator_init(size_t size) {
     // If was already initialised, then do nothing even if a different
     // size is used.
     if (memory != NULL) {
@@ -63,6 +74,7 @@ void allocator_init(u_int32_t size) {
     freelist_init(size);
 }
 
+
 // Destroy the allocator.
 void allocator_end(void) {
     free(memory);
@@ -72,11 +84,61 @@ void allocator_end(void) {
 }
 
 
+// Make me a sandwich
+void* allocator_malloc(size_t size) {
+    // Make sure the allocator has been initialised.
+    assert(memory != NULL);
+
+    // Add bookkeeping costs.
+    size += sizeof(header);
+    // printf("  looking for %u space\n", size);
+
+    // printf(">> before: "); freelist_print();
+
+    // Find a free chunk
+    Header chunk = freelist_bestfit(size);
+    if (chunk == NULL) {
+        return NULL;
+    }
+
+    // Ensure that the chunk we received was valid.
+    assert(chunk_is_free(chunk));
+    assert(size <= chunk->size);
+
+    // Perform the split, or resize the request to attain a perfect fit.
+    if (size + sizeof(header) > chunk->size) {
+        size = chunk->size;
+    } else {
+        freelist_split_chunk(chunk, size);
+        assert(chunk->size >= size);
+    }
+
+    // But if this is the only chunk in the free list, bail out.
+    if (freelist_has_one_chunk()) {
+        // This must be true, also.
+        assert(chunk == freelist_head);
+
+        return NULL;
+    }
+
+    // Mark the chunk as used.
+    chunk->magic = HEADER_USED_MAGIC;
+
+    // Remove it from the free list.
+    freelist_extract_chunk(chunk);
+
+    // printf("<< after: "); freelist_print();
+
+    return get_user_memory(chunk);
+}
+
+
 ////////////////////////////////////////////////////////////////////////
 //
 // Internal funcs start here
 //
 ////////////////////////////////////////////////////////////////////////
+
 
 // Rounds up an integer to the next power of two.
 // Undefined behaviour occurs when 0 is given.
@@ -100,10 +162,129 @@ static size_t round_up_power_of_two(size_t num) {
     return num;
 }
 
+
+// Ensure that the magic field is properly set.
+// If chunk is NULL, pretend that magic matched.
+static int chunk_is_free(Header chunk) {
+    return chunk == NULL || chunk->magic == HEADER_FREE_MAGIC;
+}
+
+static int chunk_is_used(Header chunk) {
+    return chunk == NULL || chunk->magic == HEADER_USED_MAGIC;
+}
+
+
+// Set up a chunk at a specified address.
+static Header chunk_create(void* where, size_t size) {
+    Header chunk = where;
+    chunk->magic = HEADER_FREE_MAGIC;
+    chunk->size  = size;
+    chunk->prev  = NULL;
+    chunk->next  = NULL;
+    return chunk;
+}
+
+
+// Returns usable memory pertaining to a given chunk.
+static void* get_user_memory(Header chunk) {
+    return (void*)chunk + sizeof(*chunk);
+}
+
+
 // Initialises the free list for entire allocated region.
 static void freelist_init(size_t size) {
     freelist_head = (Header)memory;
-    freelist_head->magic = HEADER_MAGIC;
-    freelist_head->size = size;
+    chunk_create(memory, size);
     freelist_head->prev = freelist_head->next = freelist_head;
+}
+
+
+// Check if the free list has only one chunk in it.
+// This goes with the invariant as per the spec.
+static int freelist_has_one_chunk(void) {
+    return  freelist_head == freelist_head->prev &&
+            freelist_head == freelist_head->next;
+}
+
+
+// Removes a given chunk from the free list.
+// Assumes that this is not the only chunk in the list.
+static void freelist_extract_chunk(Header chunk) {
+    assert(!freelist_has_one_chunk());
+
+    // We got to check if this is the head of the list, and if so,
+    // we must update it.
+    if (chunk == freelist_head) {
+        freelist_head = freelist_head->next;
+    }
+
+    chunk->prev->next = chunk->next;
+    chunk->next->prev = chunk->prev;
+
+    chunk->prev = NULL;
+    chunk->next = NULL;
+}
+
+
+// Locates a chunk that is sufficiently large for a requested size.
+// Returns pointer to a corresponding header, or NULL if no chunk was
+// large enough.
+// Assumes the size of the header has already been accounted for.
+static Header freelist_bestfit(size_t size) {
+    Header chunk = freelist_head;
+    Header best_fit = NULL;
+
+    // Styleguide is against do-while. Bite me.
+    // Go through all chunks in the free list.
+    do {
+        // If we can fit it
+        if (chunk->size >= size) {
+            // See if it is better than what we had before.
+            if (best_fit == NULL || chunk->size < best_fit->size) {
+                best_fit = chunk;
+            }
+        }
+
+        chunk = chunk->next;
+    } while(chunk != freelist_head);
+
+    return best_fit;
+}
+
+
+// Splits a given chunk so that the requested size can fit.
+// The spec says to do it using the halving method... wtf?
+static void freelist_split_chunk(Header chunk, size_t size) {
+    // Keep splitting while the chunk isn't "tight" enough.
+    while (chunk->size / 2 >= size) {
+        // Work out where to split.
+        size_t split_size = chunk->size / 2;
+        void* middle = (void*)chunk + split_size;
+
+        // Create a new chunk on the right.
+        Header right = chunk_create(middle, split_size);
+
+        // Point new chunk to correct neighbours.
+        right->next = chunk->next;
+        right->prev = chunk;
+        // Update old adjacent chunks to point properly.
+        right->next->prev = right;
+        right->prev->next = right;
+
+        // Update the size of current chunk.
+        chunk->size = split_size;
+    }
+}
+
+
+// Displays the free list; for debugging.
+static void __attribute__((unused)) freelist_print(void) {
+    Header curr = freelist_head;
+
+    do {
+        printf("<%u> ", curr->size);
+        curr = curr->next;
+    } while (curr != freelist_head);
+
+    printf("\n");
 }
--- asst1/test_allocator.c
+++ asst1/test_allocator.c
@@ -4,12 +4,19 @@
 // Summary: Assignment 1, test module for the memory allocator
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include "allocator.h"
 
-// Test prototypes
-static void testAllocatorSetup(void);
+typedef u_int32_t size_t;
 
+// Test prototypes
+static void test_allocator_setup(void);
+static void test_allocator_malloc(void);
+
+// Utility prototypes
+static void* malloc_check(size_t size);
+static void  malloc_check_fail(size_t size);
 
 ////////////////////////////////////////////////////////////////////////
 //
@@ -19,14 +26,19 @@ static void testAllocatorSetup(void);
 
 
 int main() {
-    testAllocatorSetup();
+    // Don't buffer output; ignore this.
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+
+    test_allocator_setup();
+    test_allocator_malloc();
 
     return EXIT_SUCCESS;
 }
 
 
 // Tests that _init() and _end() do not crash or raise any errors.
-static void testAllocatorSetup(void) {
+static void test_allocator_setup(void) {
     printf("Testing allocator set-up sequence\n");
         printf("  allocator_init(64)\n");
         allocator_init(64);
@@ -43,4 +55,96 @@ static void testAllocatorSetup(void) {
         printf("  allocator_end()\n");
         allocator_end();
     printf("Passed\n");
+}
+
+
+// Tests that _malloc() compiles with the specificiation.
+// In particular, we CAN EXPECT to know the offset of an allocation.
+// And we also can work out how much memory to ask for.
+// For this, we need to know the size of the header struct.
+// We also test that the memory is writeable.
+#define HEADER_SIZE 16
+static void test_allocator_malloc(void) {
+    printf("Testing allocator_malloc()...\n");
+        printf("  setting up allocator of size 256\n");
+        allocator_init(256);
+
+        printf("  malloc entire usable memory, fails the invariant\n");
+        malloc_check_fail(256 - HEADER_SIZE);
+        printf("  malloc half of usable memory\n");
+        malloc_check(128 - HEADER_SIZE);
+        printf("  now allocate 1 extra byte\n");
+        malloc_check(1);
+
+        printf("  reset\n");
+        allocator_end();
+        allocator_init(256);
+
+        printf("  malloc a half\n");
+        malloc_check((256-2*HEADER_SIZE)/2);
+        printf("  and again, fails the invariant\n");
+        malloc_check_fail((256-2*HEADER_SIZE)/2);
+
+        printf("  reset\n");
+        allocator_end();
+        allocator_init(256);
+
+        printf("  malloc in descending powers of 2\n");
+            printf("    128... ");
+            malloc_check(128 - HEADER_SIZE);
+            printf("64... ");
+            malloc_check(64 - HEADER_SIZE);
+            printf("32... ");
+            malloc_check(32 - HEADER_SIZE);
+            printf("16... ");
+            malloc_check(16 - HEADER_SIZE);
+        printf("ok\n");
+
+        printf("  reset with 4096\n");
+        allocator_end();
+        allocator_init(4096);
+
+        printf("  malloc in random powers of 2\n");
+            printf("    512... ");
+            malloc_check(512 - HEADER_SIZE);
+            printf("64... ");
+            malloc_check(64 - HEADER_SIZE);
+            printf("1024... ");
+            malloc_check(1024 - HEADER_SIZE);
+            printf("128... ");
+            malloc_check(128 - HEADER_SIZE);
+            printf("32... ");
+            malloc_check(32 - HEADER_SIZE);
+            printf("2048... ");
+            malloc_check(2048 - HEADER_SIZE);
+            printf("256... ");
+            malloc_check(256 - HEADER_SIZE);
+            printf("16... ");
+            malloc_check(16 - HEADER_SIZE);
+            printf("16... ");
+            malloc_check_fail(16-HEADER_SIZE);
+        printf("ok\n");
+
+    printf("Passed\n");
+}
+
+
+// Tries to allocate memory, does write tests, but does not free it.
+// Expects that the allocation will always succeed.
+// Returns the allocated region address.
+static void* malloc_check(size_t size) {
+    void* mem = allocator_malloc(size);
+    assert(mem != NULL);
+
+    // This will crash if we were given bogus memory.
+    memset(mem, 0xFF, size);
+
+    return mem;
+}
+
+// Tries to allocate memory.
+// Expects failure.
+static void malloc_check_fail(size_t size) {
+    void* mem = allocator_malloc(size);
+    assert(mem == NULL);
 }
