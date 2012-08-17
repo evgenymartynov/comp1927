afc00fa02e802477ede5e90437377119b5921f35
Evgeny Martynov <evgeny@epochfail.com>
2012-08-17 22:52:00 +1000

    Lab05: Change sorting to be awesome

--- lab05/sorting/run.sh
+++ lab05/sorting/run.sh
@@ -1,79 +1,110 @@
 #!/bin/bash
 
-function runrand() {
+# Terminate on errors.
+set -e
+
+# When you press ^C, this will terminate all running instances of isort.
+# Otherwise you might end up with like 20 sorters running in parallel.
+# Not pretty.
+trap "rm -f times; killall ./isort" INT
+
+OUTPUT_TABLE="times"
+
+function compile() {
+    gcc -Wall -Werror -O0    isort.c   -o isort
+    gcc -Wall -Werror -O0    genrandom.c   -o genrandom
+}
+
+# Magic. Black, black magic.
+function run_test() {
+    (time ./isort < $1 > /dev/null 2>/dev/null) 2>&1    \
+        | grep -E '^real|^sys'                          \
+        | tr -d '\n real sys'                           \
+        | sed -r 's#\t#/#2'                             \
+        | xargs echo >> $OUTPUT_TABLE
+}
+
+# Attempt to parallelise the tests to save time.
+function run_batch() {
+    echo "Running n=$n with $1"
+    echo "$n $1" >> $OUTPUT_TABLE
+
+    for i in {1..5}; do
+        for j in {1..2}; do
+            ($1 $n; echo -n .) &
+        done
+        wait
+    done
+
+    echo
+}
+
+function Random() {
     n="$1"
     f="$(mktemp)"
 
-    echo "$n" | ./gen > $f 2>/dev/null
-    time ./sort < $f > /dev/null 2>/dev/null
+    echo "$n" | ./genrandom > $f 2>/dev/null
+    run_test $f
 
     rm $f
 }
 
-function runordered() {
+function Ordered() {
     n="$1"
     f="$(mktemp)"
 
     echo $n > $f
-    echo "$n" | ./gen 2>/dev/null | tail -n +2 | sort -n >> $f
-    time ./sort < $f > /dev/null 2>/dev/null
+    echo "$n" | ./genrandom 2>/dev/null | tail -n +2 | sort -n >> $f
+    run_test $f
 
     rm $f
 }
 
-function runrev() {
+function Reversed() {
     n="$1"
     f="$(mktemp)"
 
     echo $n > $f
-    echo "$n" | ./gen 2>/dev/null | tail -n +2 | sort -n | tac >> $f
-    time ./sort < $f > /dev/null 2>/dev/null
+    echo "$n" | ./genrandom 2>/dev/null | tail -n +2 | sort -n | tac >> $f
+    run_test $f
 
     rm $f
 }
 
-# n=10000
-# echo "$n random"
-# for i in {0..10}; do
-#     runrand $n
-# done
-# echo
-# 
-# n=100000
-# echo "$n random"
-# for i in {0..3}; do
-#     runrand $n &
-#     runrand $n &
-#     runrand $n
-# done
+function present_table() {
+    python - << EOF
+L = map(str.strip, open("$OUTPUT_TABLE", "r").readlines())
+columns = []
+for l in L:
+    if '/' not in l:
+        columns.append([])
+    columns[-1].append(l)
 
-# n=10000
-# echo "$n ordered"
-# for i in {0..10}; do
-#     runordered $n
-# done
-# echo
-# 
-# n=100000
-# echo "$n ordered"
-# for i in {0..3}; do
-#     runordered $n &
-#     runordered $n &
-#     runordered $n
-# done
+numrows = len(columns[0])
+numcols = len(columns)
+widths = [max(16, len(columns[i][0])) for i in xrange(numcols)]
 
+for y in xrange(numrows):
+    print '| %2.1d  ' % y if y else '|= Run',
+    for x in xrange(numcols):
+        print ('|%s %%%ds' % (' ' if y else '=', widths[x])) % columns[x][y],
+    print
+EOF
+}
 
-n=10000
-echo "$n rev"
-for i in {0..10}; do
-    runrev $n
+#
+# This bit runs the tests:
+#
+
+compile
+rm -f $OUTPUT_TABLE
+
+for t in Random Ordered Reversed; do
+    n=10000
+    run_batch $t
+    n=100000
+    run_batch $t
 done
-echo
 
-n=100000
-echo "$n rev"
-for i in {0..3}; do
-    runrev $n &
-    runrev $n &
-    runrev $n
-done
+present_table
+rm -f $OUTPUT_TABLE
