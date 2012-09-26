f627786af8ace7e8a92b3bf1867ed5a09f66e15e
Evgeny Martynov <evgeny@epochfail.com>
2012-09-26 12:50:56 +1000

    Asst2: Add tests for asst2

---
+++ asst2/tests/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+wd=$(pwd)
+dir=$(dirname $0)
+cd "$dir"
+bin="$wd/$1"
+fmtstr="%-30s %b\n"
+
+DEF="\033[1;0m"
+FAIL="[\033[1;31mFAIL$DEF]"
+PASS="[\033[1;32mPASS$DEF]"
+
+if [[ ! -x $bin ]]; then echo "Not executable: $bin"; exit 1; fi
+
+for fname in inputs/0*; do
+    f=$(basename $fname)
+
+    if "$bin" -g < "$fname" > /dev/null && diff -y --suppress-common-lines -W 200 image.svg "outputs/$f"; then
+        printf "$fmtstr" $f "$PASS"
+    else
+        printf "$fmtstr" $f "$FAIL"
+    fi
+done
